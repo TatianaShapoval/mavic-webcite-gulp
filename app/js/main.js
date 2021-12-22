7923be550e454d920e0bf3523fe64e9fda64b361
TatianaShapoval <questopunto@gmail.com>
2021-12-22 22:44:17 +0200

    fixed slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 $(function(){
     $('.products__slider').slick({
-        
+        prevArrow: '<button class="slider-btn slider-btn__left"><img src="../app/images/icons/arroe-left.svg" alt=""></button>',
+        nextArrow: '<button class="slider-btn slider-btn__right"><img src="../app/images/icons/arrow-right.svg" alt=""></button>'
     });
 
     $('.questions__item-title').on('click', function(){
